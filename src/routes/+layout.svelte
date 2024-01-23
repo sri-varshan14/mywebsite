794cc76c4912a901d1276b1d883a892786551d8b
sri-varshan14 <mailmeatsri14@gmail.com>
2024-01-24 04:40:45 +0530

    fix: removing x-scrollbar in some layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <svelte:body class="ctp-mocha" />
-<div class="bg-ctp-crust text-ctp-text w-screen">
+<div class="bg-ctp-crust text-ctp-text">
 	<div
 		class="min-h-screen flex justify-center py-[14vh] sm:py-[6vh] md:py-[6vh] sm:flex-col md:flex-col px-3 md:px-16"
 	>
